26327c920ca18e04438ba4c77f70cb9ba70059b8
Narastro <advice02@naver.com>
2021-07-15 21:19:18 +0900

    State Control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
   render() {
-    return <h1>Hello!! {this.state.count}</h1>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
